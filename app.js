32ed9d7c241c38ad7edb7c977bc6add245d2cc23
DavideLopez <davidelopez7@gmail.com>
2022-08-05 12:59:03 +0200

    aggiunti elementi nel js

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
 console.log('socials')
+
+//creare una struttura dati
+const posts = [
+    {
+        id: 1,
+        Autore: {
+            nome: 'pablo escobar',
+            foto: 'https://picsum.photos/70/70',
+        },
+        data: '08-05-2022',
+        contenuto: 'lorem ipsum dolor sit amet',
+        media: 'https://picsum.photos/600/450',
+        like: 50,
+    },
+    {
+        id: 2,
+        Autore: {
+            nome: 'Davide Lopez',
+            foto: 'https://picsum.photos/70/70',
+        },
+        data: '08-05-2022',
+        contenuto: 'lorem ipsum dolor sit amet',
+        media: 'https://picsum.photos/600/450',
+        like: 8,
+    },
+    {
+        id: 3,
+        Autore: {
+            nome: 'Elon Musk',
+            foto: 'https://picsum.photos/70/70',
+        },
+        data: '08-05-2022',
+        contenuto: 'lorem ipsum dolor sit amet',
+        media: 'https://picsum.photos/600/450',
+        like: 999,
+    },
+];
+
